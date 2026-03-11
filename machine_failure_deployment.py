6eb7808d4dab71a5f43bc50d282b04e0c018adb0
Dipesh Shere <dipeshshere2121@gmail.com>
2026-03-11 14:17:51 +0530

    Update machine_failure_deployment.py

--- machine_failure_deployment.py
+++ machine_failure_deployment.py
@@ -19,7 +19,7 @@ CS = st.number_input("CS")
 VOC = st.number_input("VOC")
 RP = st.number_input("RP")
 IP = st.number_input("IP")
-Temperature = st.number_input("Temperature")
+temperature = st.number_input("Temperature")
 
 # Create dataframe
 df = pd.DataFrame({
@@ -31,7 +31,7 @@ df = pd.DataFrame({
     "VOC": [VOC],
     "RP": [RP],
     "IP": [IP],
-    "Temperature": [Temperature]
+    "Temperature": [temperature]
 })
 
 # Prediction
